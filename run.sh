aa548e5dc553991036c72342795cc91e37eaacec
Atharv Darekar <63006068+ninja-con-gafas@users.noreply.github.com>
2023-02-19 19:51:35 +0530

    fix: loss of data by starting the Apache Spark Streaming application first

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-python3 "simulator/MQTTSparkplugBDataGenerator.py" "simulator/mqtt_sparkplug_b_data_generator_configuration.json" &
-python3 "processor/DataStreamer.py" &
+echo "Start the Apache Spark Streaming Application"
 spark-submit --packages org.apache.bahir:spark-streaming-mqtt_2.11:2.4.0 "laboratory/experiment/read_mqtt_stream.py" &
+echo "Wait for 30 seconds to get the application running"
+sleep 30
+
+echo "Start the Sparkplug B MQTT IoT device simulator"
+python3 "simulator/MQTTSparkplugBDataGenerator.py" "simulator/mqtt_sparkplug_b_data_generator_configuration.json" &
+
+echo "Start the data streamer"
+python3 "processor/DataStreamer.py" &
 wait
